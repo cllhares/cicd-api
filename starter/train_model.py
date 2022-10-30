f419952b3c1584bca1d698be0f68a6e140a8ef55
cllhares <canteen@buffalo.edu>
2022-10-29 23:09:36 -0400

    Added Functions to Load Data, and Train Model

---
+++ starter/train_model.py
@@ -0,0 +1,40 @@
+# Script to train machine learning model.
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import joblib
+
+# Add the necessary imports for the starter code.
+from ml.data import process_data, load_data
+from ml.model import train_model
+
+# Add code to load in the data.
+data = pd.read_csv("./data/clean_data_census.csv")
+
+print(data.head())
+
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
+
+# Proces the test data with the process_data function.
+# Train and save a model.
+print("Saving Model...")
+model = train_model(X_train, y_train)
+joblib.dump(model, "./model/census-lr-model.pkl")
+joblib.dump(encoder, "./model/census-lr-encoder-model.pkl")
+joblib.dump(lb, "./model/census-lr-lb-model.pkl")
+
+
